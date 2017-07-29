73e7850eeee0696c95d79d478ed4ebc5b99e0717
chuanchina@yeah.net <hinini@foxmail.com>
2017-07-29 08:24:34 +0800

    ID1006

---
+++ PAT/BasicLevel/P1006.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * Created by q on 17-7-29.
+ */
+public class P1006 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+
+        for (int i = 0; i < num / 100; i++) {
+            System.out.print("B");
+        }
+        for (int i = 0; i < (num / 10) % 10; i++) {
+            System.out.print("S");
+        }
+        for (int i = 0; i < num % 10; i++) {
+            System.out.print(i + 1);
+        }
+
+    }
+}

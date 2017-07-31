0ff2895edf14af9a593c6f28777de6d92381f00d
chuanchina@yeah.net <hinini@foxmail.com>
2017-07-31 23:59:31 +0800

    ID1016

---
+++ PAT/BasicLevel/P1016.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by q on 17-7-31.
+ */
+public class P1016 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String[] strings = bufferedReader.readLine().split(" ");
+
+        int paNum = thePCount(strings[0], strings[1]);
+        int pbNum = thePCount(strings[2], strings[3]);
+
+        int pa = the1Num(paNum);
+        int pb = the1Num(pbNum);
+
+        System.out.println(pa * Integer.parseInt(strings[1]) + pb * Integer.parseInt(strings[3]));
+
+    }
+
+    public static int thePCount(String s1, String s2) {
+        int i = 0;
+        for (int j = 0; j < s1.length(); j++) {
+            if (s1.charAt(j) == s2.charAt(0)) {
+                i++;
+            }
+        }
+        return i;
+    }
+
+    public static int the1Num(int n) {
+        int num = 0;
+        for (int i = 0; i < n; i++) {
+            num += Math.pow(10, i);
+        }
+        return num;
+    }
+}

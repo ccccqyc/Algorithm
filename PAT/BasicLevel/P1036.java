e7ee35946112d827a12a31fc513623d88051b1da
chuanchina@yeah.net <hinini@foxmail.com>
2017-08-01 22:39:45 +0800

    ID1036

---
+++ PAT/BasicLevel/P1036.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by q on 17-8-1.
+ */
+public class P1036 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String s = bufferedReader.readLine();
+        bufferedReader.close();
+
+        int edge = Integer.parseInt(s.split(" ")[0]);
+        char c = String.valueOf(s.split(" ")[1]).charAt(0);
+
+        for (int j = 0; j < Math.round(edge / 2.0); j++) {
+            for (int i = 0; i < edge; i++) {
+                if (j == 0 || j == Math.round(edge / 2.0) - 1) {
+                    System.out.print(c);
+                } else {
+                    if (i == 0 || i == edge - 1) {
+                        System.out.print(c);
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}

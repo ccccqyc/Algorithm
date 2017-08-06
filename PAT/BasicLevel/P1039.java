bdcdbcd1034b448d4b230a1dfd107764d3bc0bd8
chuanchina@yeah.net <hinini@foxmail.com>
2017-08-07 00:17:50 +0800

    ID1039

--- PAT/BasicLevel/P1039.java
+++ PAT/BasicLevel/P1039.java
@@ -9,16 +9,16 @@ public class P1039 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] chars1 = bufferedReader.readLine().split("");
-        String[] chars2 = bufferedReader.readLine().split("");
+        char[] chars1 = bufferedReader.readLine().toCharArray();
+        char[] chars2 = bufferedReader.readLine().toCharArray();
 
         int lengthChars1 = chars1.length;
         int lengthChars2 = chars2.length;
 
-        for (String char2 : chars2) {
-            for (int i=0;i<lengthChars1;i++) {
+        for (Character char2 : chars2) {
+            for (int i = 0; i < lengthChars1; i++) {
                 if (char2.equals(chars1[i])) {
-                    chars1[i] = "";
+                    chars1[i] = ' ';
                     lengthChars2--;
                     break;
                 }

7cac44b7d5b7f7782bfc3904b9464a112c5f7d39
ccccqyc <chuancnw@yeah.net>
2017-08-09 19:36:52 +0800

    ID1057

--- PAT/BasicLevel/P1057.java
+++ PAT/BasicLevel/P1057.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
+
 /**
  * Created by q on 17-8-9.
  */
 public class P1057 {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String string = in.next();
+        in.close();
 
     }
 }

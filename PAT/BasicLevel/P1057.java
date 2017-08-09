85d77def3d3db641d2f5ae093b9afc2692daa49e
ccccqyc <chuancnw@yeah.net>
2017-08-09 23:04:22 +0800

    ID1057

--- PAT/BasicLevel/P1057.java
+++ PAT/BasicLevel/P1057.java
@@ -1,13 +1,50 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by q on 17-8-9.
  */
 public class P1057 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String string = in.next();
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        char[] chars = in.readLine().toLowerCase().toCharArray();
         in.close();
 
+        List<Character> list = new ArrayList<>();
+        for (char c : chars) {
+            if (c >= 97 && c <= 122) {
+                list.add(c);
+            }
+        }
+
+        Iterator<Character> iterator = list.iterator();
+        int sum = 0;
+        while (iterator.hasNext()) {
+            sum += ((int) iterator.next() - 96);
+        }
+        int zeroCount = 0;
+        int oneCount = 0;
+//        String[] binaryInts = Integer.toBinaryString(sum).split("");
+//        for (String s : binaryInts) {
+//            if (s.equals("0")) {
+//                zeroCount++;
+//            } else {
+//                oneCount++;
+//            }
+//        }
+
+        while(sum!=0){
+            if (sum%2!=0){
+                oneCount++;
+            }else {
+                zeroCount++
+            ;}
+            sum /=2;
+        }
+        System.out.print(zeroCount + " " + oneCount);
     }
 }

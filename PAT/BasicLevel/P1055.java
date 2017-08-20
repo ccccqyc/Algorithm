d5a8418683a4577dcd33b7267884f57cd6ab21a5
chuan <chuanchina@yeah.net>
2017-08-20 20:29:56 +0800

    ID1055

--- PAT/BasicLevel/P1055.java
+++ PAT/BasicLevel/P1055.java
@@ -4,34 +4,91 @@ import java.util.List;
 import java.util.Scanner;
 
 /**
+ * 测试点 3.4.5超时
  * Created by q on 17-8-15.
  */
 public class P1055 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        //人数
         int peopleNum = in.nextInt();
+        //排数
         int row = in.nextInt();
         in.nextLine();
-        String[][] people = new String[peopleNum][2];
-        int[] heigh = new int[peopleNum];
+        People[] peoples = new People[peopleNum];
         for (int i = 0; i < peopleNum; i++) {
-            people[i] = in.nextLine().split(" ");
-            heigh[i] = Integer.parseInt(people[i][1]);
+            peoples[i] = new People(in.next(), in.nextInt());
         }
         in.close();
+        Arrays.sort(peoples);
 
+        //每排人数
         int rowPeoNum = peopleNum / row;
-        Arrays.sort(heigh);
-        int[] subRowPeoNum = Arrays.copyOfRange(heigh, (row-1)*rowPeoNum, heigh.length);
+        People[] subRowPeoNum = Arrays.copyOfRange(peoples, (row - 1) * rowPeoNum, peoples.length);
 
-        List<int[]> list = new ArrayList<>();
-        for (int i = (row-1)*rowPeoNum-1; i > 0; i -= rowPeoNum) {
+        List<People[]> list = new ArrayList<>();
+        for (int i = (row - 1) * rowPeoNum; i > 0; i -= rowPeoNum) {
             list.add(subRowPeoNum);
-            subRowPeoNum = Arrays.copyOfRange(heigh,  i - rowPeoNum,i);
+            subRowPeoNum = Arrays.copyOfRange(peoples, i - rowPeoNum, i);
         }
         list.add(subRowPeoNum);
+        //临时数组
+        People[] temp;
+        //每排最高的人
+        int hightPeo;
+        int right;
+        int left;
 
+        for (People[] aList : list) {
+            subRowPeoNum = aList;
+            temp = subRowPeoNum.clone();
+            //利用除法,数组从零开始这两个特性
+            hightPeo = subRowPeoNum.length / 2;
+            right = hightPeo + 1;
+            left = hightPeo - 1;
+            temp[hightPeo] = subRowPeoNum[subRowPeoNum.length - 1];
+            for (int j = subRowPeoNum.length - 2; j >= 0; j--) {
+                temp[left--] = subRowPeoNum[j];
+                if (j > 0) {
+                    temp[right++] = subRowPeoNum[--j];
+                }
+            }
+            for (int j = 0; j < temp.length; j++) {
+                System.out.print(temp[j].getName());
+                if (j != subRowPeoNum.length - 1) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
 
+class People implements Comparable<People> {
+    private String name;
+    private int high;
 
+    People(String name, int high) {
+        this.name = name;
+        this.high = high;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    @Override
+    /*
+     定义排序特性.
+      身高增序,身高同则姓名字典序.
+     */
+    public int compareTo(People people) {
+        if (this.high < people.high) {
+            return -1;
+        } else if (this.high > people.high) {
+            return 1;
+        } else {
+            return people.name.compareTo(this.name);
+        }
     }
 }

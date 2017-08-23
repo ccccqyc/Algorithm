4027ec3d1a1596e2f4158fea46e493f51d1ff1ff
ccccqyc <chuancnw@yeah.net>
2017-08-23 22:05:27 +0800

    ID1001

--- PAT/Advanced/A1001.java
+++ PAT/Advanced/A1001.java
@@ -1,31 +1,32 @@
-    import java.util.Scanner;
+import java.util.Scanner;
 
-    /**
-     * Created by q on 17-8-23.
-     */
-    public class A1001 {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-            String c = String.valueOf(Math.abs(a + b));
-            scanner.close();
+/**
+ * All pass.
+ * Created by q on 17-8-23.
+ */
+public class A1001 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        String c = String.valueOf(Math.abs(a + b));
+        scanner.close();
 
-            StringBuilder s = new StringBuilder("");
-            int length = c.length();
-            s.append(String.copyValueOf(c.toCharArray(), 0, length % 3));
-            if (length % 3 != 0 && length > 3) {
+        StringBuilder s = new StringBuilder("");
+        int length = c.length();
+        s.append(String.copyValueOf(c.toCharArray(), 0, length % 3));
+        if (length % 3 != 0 && length > 3) {
+            s.append(',');
+        }
+        for (int i = c.length() % 3; i < length; i += 3) {
+            s.append(String.copyValueOf(c.toCharArray(), i, 3));
+            if (i != length - 3) {
                 s.append(',');
             }
-            for (int i = c.length() % 3; i < length; i += 3) {
-                s.append(String.copyValueOf(c.toCharArray(), i, 3));
-                if (i != length - 3) {
-                    s.append(',');
-                }
-            }
-            if (a + b < 0) {
-                s.insert(0, '-');
-            }
-            System.out.println(s);
         }
+        if (a + b < 0) {
+            s.insert(0, '-');
+        }
+        System.out.println(s);
     }
+}

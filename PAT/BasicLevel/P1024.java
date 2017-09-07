84218ec5566d3156ebf0b7b09c0992aac2e84d61
ccccqyc <chuancnw@yeah.net>
2017-09-07 16:49:18 +0800

    ID1024

--- PAT/BasicLevel/P1024.java
+++ PAT/BasicLevel/P1024.java
@@ -1,27 +1,43 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /**
- * 测试5点 错误
+ * 测试点6超时
  * Created by q on 17-8-28.
  */
 public class P1024 {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String[] ints = bufferedReader.readLine().split("[E]");
-        bufferedReader.close();
+        Scanner scanner = new Scanner(System.in);
+        String[] ints = scanner.nextLine().split("[\\.E]");
+        scanner.close();
+
         StringBuilder stringBuilder = new StringBuilder("");
         if (ints[0].charAt(0) == '-') {
-            System.out.print("-");
+            stringBuilder.append("-");
         }
 
         ints[0] = ints[0].substring(1, ints[0].length());
-        int decimalCount = ints[0].split("\\.")[1].length() - Integer.parseInt(ints[1]);
-        if (decimalCount < 0) {
-            decimalCount = 0;
+        int lengthDec = ints[1].length();
+        int count10 = Integer.parseInt(ints[2]);
+        if (count10 >= 0) {
+            stringBuilder.append(ints[0]);
+            if (count10 >= lengthDec) {
+                stringBuilder.append(ints[1]);
+                for (int i = count10 - lengthDec; i > 0; i--) {
+                    stringBuilder.append("0");
+                }
+            } else if (count10 < lengthDec) {
+                stringBuilder.append(ints[1].substring(0, count10) + "." + ints[1].substring(count10, lengthDec));
+            }
+        } else if (count10 < 0) {
+            stringBuilder.append("0.");
+            for (int i = -count10 - 1; i > 0; i--) {
+                stringBuilder.append("0");
+            }
+            stringBuilder.append(ints[0] + ints[1]);
         }
-        System.out.printf("%." + decimalCount + "f", (Double.parseDouble(ints[0]) * Math.pow(10, Integer.parseInt(ints[1]))));
+
+        System.out.print(stringBuilder);
 
     }
 }

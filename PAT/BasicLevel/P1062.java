77cc0254ded8ed83baed180721f68ec7cea940a1
ccccqyc <chuancnw@yeah.net>
2017-09-09 18:21:37 +0800

    ID1062

--- PAT/BasicLevel/P1062.java
+++ PAT/BasicLevel/P1062.java
@@ -10,15 +10,15 @@ import java.util.Scanner;
 public class P1062 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] string = scanner.nextLine().replace("/", " ").split(" ");
+        String[] string = scanner.nextLine().split("[\\/ ]");
         scanner.close();
 
         int[] ints = new int[5];
         for (int i = 0; i < 5; i++) {
             ints[i] = Integer.parseInt(string[i]);
         }
-        double a = Math.ceil(ints[0] * ints[4] / (float) ints[1]);
-        double b = Math.floor(ints[2] * ints[4] / (float) ints[3]);
+        double a = ints[0] * ints[4] / (double) ints[1];
+        double b = ints[2] * ints[4] / (double) ints[3];
         if (a > b) {
             double temp = a;
             a = b;
@@ -26,8 +26,8 @@ public class P1062 {
         }
 
         List<Integer> list = new LinkedList<>();
-        for (int i = (int) a; i <= b; i++) {
-            if (hasComDivr(ints[4], i)) {
+        for (int i = (int) (a + 1); i <= b; i++) {
+            if (!hasComDivr(ints[4], i)) {
                 list.add(i);
             }
         }
@@ -40,10 +40,10 @@ public class P1062 {
     }
 
     private static boolean hasComDivr(int m, int n) {
-        boolean b = true;
+        boolean b = false;
         for (int i = 2; i < m; i++) {
             if (n % i == 0 && m % i == 0) {
-                b = false;
+                b = true;
                 break;
             }
         }

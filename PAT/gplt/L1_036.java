742c5063c3abaa458507167683ac235170e49bb2
chuan <chuanchina@yeah.net>
2017-09-15 10:22:29 +0800

    L1-036

---
+++ PAT/gplt/L1_036.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * all pass
+ * Created by q on 17-9-15.
+ */
+public class L1_036 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(scanner.nextInt() * scanner.nextInt());
+        scanner.close();
+    }
+}

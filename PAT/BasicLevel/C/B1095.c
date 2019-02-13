9d4ee2c07b1bfbec7f24b4e24cf452271a23e031
chuan <chuanchina@yeah.net>
2019-02-13 10:44:47 +0800

    some new

---
+++ PAT/BasicLevel/C/B1095.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int stu, com, i;
+    scanf("%d %d", &stu, &com);
+    int stus[2][stu], coms[2][com];
+    for (i = 0; i < stu; i++)
+    {
+        
+    }
+
+    return 0;
+}

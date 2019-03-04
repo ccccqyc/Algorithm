3dc2102cb74c15bb71d4a62abea9ce87ca5f71dd
ccccqyc <chuancnw@yeah.net>
2019-03-04 19:34:05 +0800

    bs

--- PAT/BasicProgram/p6_10.c
+++ PAT/BasicProgram/p6_10.c
@@ -10,3 +10,21 @@ int main()
     Print_Factorial(N);
     return 0;
 }
+
+void Print_Factorial ( const int N ){
+	long temp=1l;
+	
+	if(N<0){
+		printf("Invalid input");
+		return;
+	}else if(N==0){
+		temp = 1;
+	}else{
+		int i = 1;
+		for(i;i<=N;i++){
+			temp *=i;
+		}
+	}
+	
+	printf("%d",temp);
+}

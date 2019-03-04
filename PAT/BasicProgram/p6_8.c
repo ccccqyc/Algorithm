16cffe981aa41164c92a92c5703ce553472eac39
ccccqyc <chuancnw@yeah.net>
2019-03-04 19:02:12 +0800

    commit the last

---
+++ PAT/BasicProgram/p6_8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int Factorial( const int N );
+
+int main()
+{
+    int N, NF;
+	
+    scanf("%d", &N);
+    NF = Factorial(N);
+    if (NF)  printf("%d! = %d\n", N, NF);
+    else printf("Invalid input\n");
+
+    return 0;
+}

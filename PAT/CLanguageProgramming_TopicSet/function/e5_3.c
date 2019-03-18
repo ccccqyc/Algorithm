4105593ca8fe20f1be34c6fad879ebf2d7d31f80
chuan <chuanchina@yeah.net>
2019-03-18 16:22:42 +0800

    c

---
+++ PAT/CLanguageProgramming_TopicSet/function/e5_3.c
@@ -0,0 +1,16 @@
+#include <math.h>
+#include <stdio.h>
+
+double dist(double x1, double y1, double x2, double y2);
+
+int main() {
+    double x1, y1, x2, y2;
+
+    scanf("%lf %lf %lf %lf", &x1, &y1, &x2, &y2);
+    printf("dist = %.2f\n", dist(x1, y1, x2, y2));
+
+    return 0;
+}
+double dist(double x1, double y1, double x2, double y2) {
+    return sqrt((pow((x2 - x1), 2) + pow((y2 - y1), 2)));
+}
---
+++ PAT/CLanguageProgramming_TopicSet/function/p5_1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int sum(int m, int n);
+
+int main() {
+    int m, n;
+
+    scanf("%d %d", &m, &n);
+    printf("sum = %d\n", sum(m, n));
+
+    return 0;
+}
+
+int sum(int m, int n) {
+    int sum = 0;
+    int i = 0;
+    for( i = m; i <=n ; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}

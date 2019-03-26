2de2f9f231123b8c7d6ea3bf732c99ff35235c67
ccccqyc <chuancnw@yeah.net>
2019-03-26 21:27:27 +0800

    update

--- PAT/QiBu/L04.c
+++ PAT/QiBu/L04.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    int i, arr[20] = {-1};
-    for (i = 0; getchar() != "\n"; i++) {
-        arr[i] = getchar();
+    //int 的范围 2^32
+    int i, length=0, arrint[20] = {-1},length2;
+    char arr[20] = {-1};
+    for (i = 0; (arr[i] = getchar()) != '\n'; i++) {
+        printf("%d", arr[i] - 48);
+        arrint[i] = arr[i] - 48;
+        length ++;
     }
 
-    printf("%d",arr[0]);
+    int num = 2,temp;
+    for( i = 0; i < length; i++)
+    {
+        temp = arrint[i] + num;
+    }
+    
+
+
+    // printf("%d", arr[0]-48);
     return 0;
 }

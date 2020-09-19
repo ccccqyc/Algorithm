1afe8b6d2262552268ae67d875af29b58bc7ba3f
ccccqyc <chuancnw@yeah.net>
2020-09-19 22:48:40 +0800

    feat:算法-Leetcode 404 sum of left leaves 左子叶之和 深度优先

---
+++ LeetCode/400_500/404SumofLeftLeaves.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isLeafNode(TreeNode* node) {
+        return !node->left && !node->right;
+    }
+
+    int dfs(TreeNode* node) {
+        int ans = 0;
+        if (node->left) {
+            ans += isLeafNode(node->left) ? node->left->val : dfs(node->left);
+        }
+        if (node->right && !isLeafNode(node->right)) {
+            ans += dfs(node->right);
+        }
+        return ans;
+    }
+
+    int sumOfLeftLeaves(TreeNode* root) {
+        return root ? dfs(root) : 0;
+    }
+};
+

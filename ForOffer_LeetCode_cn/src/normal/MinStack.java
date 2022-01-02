abf82c368308df49c1f61d04932b656efa8f797e
ccccqyc <chuancnw@yeah.net>
2022-01-02 11:45:18 +0800

    剑指offer 30. 包含min函数的栈

---
+++ ForOffer_LeetCode_cn/src/normal/MinStack.java
@@ -0,0 +1,57 @@
+package normal;
+
+import java.util.Stack;
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.min();
+ *
+ * @author ccccqyc
+ * @date : 2022/01/02/10:51
+ */
+public class MinStack {
+    private Node head;
+
+    /**
+     * initialize your data structure here.
+     */
+    public MinStack() {
+    }
+
+    public void push(int x) {
+        if (head == null) {
+            head = new Node(x, x, null);
+        } else {
+            head = new Node(x, Math.min(x, head.min), head);
+        }
+    }
+
+    public void pop() {
+        head = head.next;
+    }
+
+    public int top() {
+        return head.val;
+    }
+
+    public int min() {
+        return head.min;
+    }
+
+    private class Node {
+        int val;
+        int min;
+        Node next;
+
+        public Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+}
+

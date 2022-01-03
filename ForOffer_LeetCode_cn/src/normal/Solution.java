e5e867888c026ea2af16a37a2d5baeedb9aa11f8
ccccqyc <chuancnw@yeah.net>
2022-01-03 15:32:49 +0800

    剑指 Offer 24. 反转链表

--- ForOffer_LeetCode_cn/src/normal/Solution.java
+++ ForOffer_LeetCode_cn/src/normal/Solution.java
@@ -9,7 +9,7 @@ import java.util.Stack;
 public class Solution {
     /**
      * 剑指 Offer 06. 从尾到头打印链表
-     *
+     * <p>
      * Definition for singly-linked list.
      * public class ListNode {
      * int val;
@@ -31,6 +31,28 @@ public class Solution {
             rets[rets.length - count--] = stack.pop();
         }
         return rets;
+    }
+
+    /**
+     * 剑指 Offer 24. 反转链表
+     * <p>
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    public ListNode reverseList(ListNode head) {
+        ListNode res = null;
+        ListNode temp;
+        while (head != null) {
+            temp = head;
+            head = head.next;
+            temp.next = res;
+            res = temp;
+        }
+        return res;
     }
 
     private class ListNode {

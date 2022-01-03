c2fafe823c1e648affa0d6ba0cf9cdd782fc45f3
ccccqyc <chuancnw@yeah.net>
2022-01-03 17:01:51 +0800

    剑指 Offer 35. 复杂链表的复制

--- ForOffer_LeetCode_cn/src/normal/Solution.java
+++ ForOffer_LeetCode_cn/src/normal/Solution.java
@@ -55,6 +55,82 @@ public class Solution {
         return res;
     }
 
+    /**
+     * 剑指 Offer 35. 复杂链表的复制
+     * <p>
+     * Definition for a Node.
+     * class Node {
+     * int val;
+     * Node next;
+     * Node random;
+     * <p>
+     * public Node(int val) {
+     * this.val = val;
+     * this.next = null;
+     * this.random = null;
+     * }
+     * }
+     */
+    public Node copyRandomList(Node head) {
+        int length = 0;
+        Node temp = head;
+        // length
+        while (temp != null) {
+            length++;
+            temp = temp.next;
+        }
+
+        Node[] nodes = new Node[length];
+
+        // create nodes
+        temp = head;
+        while (temp != null) {
+            nodes[nodes.length - length--] = new Node(temp.val);
+            temp = temp.next;
+        }
+
+        // next rondom
+        Node temp2, temp3 = head;
+        int index = 0, count = 0;
+        while (temp3 != null) {
+            temp2 = temp3.random;
+            temp = head;
+            count = 0;
+            // random
+            if (temp2 == null) {
+                nodes[index].random = null;
+            } else {
+                while (temp != temp2) {
+                    temp = temp.next;
+                    count++;
+                }
+                nodes[index].random = nodes[count];
+            }
+            // next
+            if (index < nodes.length - 1) {
+                nodes[index].next = nodes[index + 1];
+            } else {
+                nodes[index].next = null;
+            }
+
+            index++;
+            temp3 = temp3.next;
+        }
+        return (nodes.length == 0) ? null : nodes[0];
+    }
+
+    class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+
     private class ListNode {
         int val;
         ListNode next;

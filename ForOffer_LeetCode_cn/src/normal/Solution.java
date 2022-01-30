c61ba16c797c3e1274b5ffb924401b1877d05c25
ccccqyc <chuancnw@yeah.net>
2022-01-30 22:46:27 +0800

    剑指 Offer 49. 丑数

--- ForOffer_LeetCode_cn/src/normal/Solution.java
+++ ForOffer_LeetCode_cn/src/normal/Solution.java
@@ -1981,6 +1981,46 @@ public class Solution {
         return ret;
     }
 
+    /**
+     * 剑指 Offer 38. 字符串的排列
+     * <p>
+     * T(n) = O(N)
+     * S(n) = O(N)
+     *
+     * @since 2022-01-30 22:30:12
+     */
+    public String[] permutation(String s) {
+        return new String[1];
+    }
+
+    /**
+     * 剑指 Offer 49. 丑数
+     * <p>
+     * T(n) = O(N)
+     * S(n) = O(N)
+     *
+     * @since 2022-01-30 22:44:00
+     */
+    public int nthUglyNumber(int n) {
+        if (n <= 0)
+            return -1;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int id2 = 0, id3 = 0, id5 = 0;
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.min(dp[id2] * 2, Math.min(dp[id3] * 3, dp[id5] * 5));
+            // 这里不用else if的原因是有可能id2(3) * 2 == id3(2) * 3
+            // 这种情况两个指针都要后移
+            if (dp[id2] * 2 == dp[i])
+                id2 += 1;
+            if (dp[id3] * 3 == dp[i])
+                id3 += 1;
+            if (dp[id5] * 5 == dp[i])
+                id5 += 1;
+        }
+        return dp[n - 1];
+    }
+
     static class ListNode {
         int val;
         ListNode next;
